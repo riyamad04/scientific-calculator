d1abdf3b4133a1df7f84c9ae16a489355bc6d58c
Riya Sharma <riyasharmaoksbi@gmail.com>
2022-11-28 15:59:44 +0530

    scientific calculator

---
+++ script.js
@@ -0,0 +1,72 @@
+// here we have screen and btn varibales for display and buttons
+
+const screen = document.querySelector("#screen");
+const btn = document.querySelectorAll(".btn");
+
+// for getting the value of btn, we are using for loop here
+
+for (item of btn) {
+  item.addEventListener("click", (e) => {
+    btntext = e.target.innerText;
+
+    if (btntext == "x") {
+      btntext = "*";
+    }
+    if (btntext == "÷") {
+      btntext = "/";
+    }
+    screen.value += btntext;
+  });
+}
+
+// here we have made functions for all the functionalities in calculator
+
+function sin() {
+  screen.value = Math.sin(screen.value);
+}
+
+function cos() {
+  screen.value = Math.cos(screen.value);
+}
+
+function tan() {
+  screen.value = Math.tan(screen.value);
+}
+
+function pow() {
+  screen.value = Math.pow(screen.value, 2);
+}
+
+function sqrt() {
+  screen.value = Math.sqrt(screen.value);
+}
+
+// function log() {
+//   screen.value = Math.log(screen.value);
+// }
+
+function pi() {
+  screen.value = 3.14159265359;
+}
+
+function e() {
+  screen.value = 2.71828182846;
+}
+
+function fact() {
+  let i, num, f;
+  f = 1;
+  num = screen.value;
+  for (i = 1; i <= num; i++) {
+    f = f * i;
+  }
+
+  i = i - 1;
+
+  screen.value = f;
+}
+
+// function for backspace button
+function backspace() {
+  screen.value = screen.value.substr(0, screen.value.length - 1);
+}
